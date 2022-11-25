f00b734105d75aae21a0f690fd108a662b5259d0
markziemann <mark.ziemann@gmail.com>
2022-11-25 23:58:01 +1100

    added some static content protected by auth

--- website/auth.py
+++ website/auth.py
@@ -64,3 +64,8 @@ def sign_up():
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
+
+@auth.route('/static/*.html')
+@login_required
+def send_html_files(filename):
+    return send_from_directory('html_files', filename)
